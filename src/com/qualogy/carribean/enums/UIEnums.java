1e805887b4922aa55025a49eab42ca68376bf7a0
Maarten Narain <mnarain@qualogy.com>
2018-12-13 19:46:18 -0300

    static ipv instance

--- src/com/qualogy/carribean/enums/UIEnums.java
+++ src/com/qualogy/carribean/enums/UIEnums.java
@@ -7,7 +7,7 @@ public enum UIEnums {
     LOGIN,
     REGISTRATION;
 
-    public boolean existsValue(String value){
+    public static boolean existsValue(String value){
         return Arrays.stream(UIEnums.values()).anyMatch(uiEnums -> value.equals(uiEnums.name()));
     }
 }
